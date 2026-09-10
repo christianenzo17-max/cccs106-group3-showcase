1dff7664c740e99e9a00332406344ef52c365f27
Jian Alvarez <jialvarez@my.cspc.edu.ph>
2026-09-10 11:56:12 +0800

    update profile card

--- team_profiles.py
+++ team_profiles.py
@@ -45,7 +45,7 @@ def get_initial_team() -> List[TeamMember]:
             full_name="[Dev 1] Christian Enzo R. Sta. Cruz", # example: "Maria Clara Santos"
             role="Lead Frontend UI Developer",
             specialization="Flet Reactive Widgets & Material 3 Layouts",
-            github_handle="@dev1-christianenzo17-max",          # example: "@mariaclara-cspc"
+            github_handle="@Rvin1205",          # example: "@mariaclara-cspc"
             avatar_icon=ft.Icons.PALETTE,
             accent_color=ft.Colors.TEAL_700,
             photo_url="https://avatars.githubusercontent.com/u/322104563?v=4",                 # Photo: local asset or URL (e.g. "https://github.com/dev1-github.png")
@@ -58,7 +58,7 @@ def get_initial_team() -> List[TeamMember]:
             github_handle="@dev1-christianenzo17-max",          # example: "@mariaclara-cspc"
             avatar_icon=ft.Icons.PALETTE,
             accent_color=ft.Colors.TEAL_700,
-            photo_url="https://avatars.githubusercontent.com/u/322104563?v=4",                 # Photo: local asset or URL (e.g. "https://github.com/dev1-github.png")
+            photo_url="https://avatars.githubusercontent.com/u/324363537?v=4",                 # Photo: local asset or URL (e.g. "https://github.com/dev1-github.png")
         ),
         # ----------------------------------------------------------------------
         # TODO: [DEVELOPER 2 TASK] - Branch: feature/dev2-state
